7de02cbead6f53971ea4b9edeb06682744890ad6
Syllinger <syllinger@gmail.com>
2017-04-04 03:32:16 -0400

    Add state and routing logic to PromptContainer component.

--- app/config/routes.js
+++ app/config/routes.js
@@ -15,7 +15,7 @@ var routes = (
     <Route path='/' component={Main}>
       <IndexRoute component={Home} />
       <Route path='playerOne' header='Player One' component={PromptContainer} />
-      <Route path='playterTwo/:playerOne' header='Player Two' component={PromptContainer} />
+      <Route path='playerTwo/:playerOne' header='Player Two' component={PromptContainer} />
     </Route>
   </Router>
 );
