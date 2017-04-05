c94d2d27814ce26e1d7de317f868c891906f829e
Syllinger <syllinger@gmail.com>
2017-04-05 12:39:46 -0400

    Create scaffolding and routes for results page.

---
+++ app/components/Results.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var PropTypes = React.PropTypes;
+
+function Results(props) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+module.exports = Results;
---
+++ app/containers/ResultsContainer.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Results = require('../components/Results');
+
+var ResultsContainer = React.createClass({
+  render: function() {
+    return (
+      <Results />
+    );
+  }
+});
+
+module.exports = ResultsContainer;
