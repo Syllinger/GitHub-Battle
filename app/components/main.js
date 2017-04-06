13959d811513ac8c3e092bb11a83759bd15bd7f3
Syllinger <syllinger@gmail.com>
2017-04-06 00:31:21 -0400

    Add transition animations to application. Install React CSS Transition Group addon. Create css style sheet for animation reference. Wrap main container in CSSTransitionGroup component. Downgrade dependencies and devDependencies in package.json to rule out transitions not working correctly as a result of version conflicts.
    ***********************************************************************
    Observed that when rendering Prompt component for the first time, a
    duplicate version of the component is shown until animation finishes.
    ***********************************************************************
    
    NOTE [package.json]: versions of react and css-transition-group do not
    match.
    NOTE [Main.js]: React.cloneElement required to wrap this.props.children,
    as CSSTransitionGroup requires that key prop be passed into child
    component.

--- app/components/main.js
+++ app/components/main.js
@@ -1,10 +1,23 @@
 var React = require('react');
+var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
+
+require('../main.css');
 
 var Main = React.createClass({
   render: function() {
+
+    //var segment = this.props.location.pathname.split("/")[1] || "root";
+    console.log(this.props.location.pathname);
+
     return (
       <div className='main-container'>
-        {this.props.children}
+        <ReactCSSTransitionGroup
+          transitionName="appear"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={500}
+        >
+          {React.cloneElement(this.props.children, {key:this.props.location.pathname})}
+        </ReactCSSTransitionGroup>
       </div>
     );
   }
